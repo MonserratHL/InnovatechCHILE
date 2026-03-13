65ced8c343ea8d7aaafff952603029d600618f23
PAAWSA02LC1011 <Duoc@PAAWSA02LC1011.duoc.acad>
2026-03-13 09:50:54 -0300

    arreglado

--- backend/src/main/java/cl/spa/backend/model/Plan.java
+++ backend/src/main/java/cl/spa/backend/model/Plan.java
@@ -32,6 +32,43 @@ public class Plan {
         this.descripcion = descripcion;
         this.precio = precio;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Plan{" + "id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", precio=" + precio + '}';
+    }
     
     
     
