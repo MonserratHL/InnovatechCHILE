07ca10e64bcdc508bc4efd342c3275c742dd2e88
MonserratHL <mo.huarapil@duocuc.cl>
2026-04-07 16:48:08 -0400

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -11,23 +11,4 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Data source: AMI Ubuntu 22.04 LTS
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"]  # Canonical
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-# Data source: Availability Zones
-data "aws_availability_zones" "available" {
-  state = "available"
-}
